ef75884850e4af0d2aaa0d734b29ab2b9d0d56d5
lucky <luka05@bk.ru>
2025-08-25 03:36:31 +0300

    Added lose and win condition

--- src/main/java/com/github/springerris/Main.java
+++ src/main/java/com/github/springerris/Main.java
@@ -7,9 +7,11 @@ import java.util.stream.IntStream;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    private static final int[]  powers = {0,2,4,8,16,32,64,128,256,512,1024,2048};
-    private static int WIDTH = 4;
-    private static int HEIGHT = 4;
+    private static final int[]  powers = {4,8,16,32,64,128,256,512,1024,2048};
+    private static final int WIDTH = 4;
+    private static final int HEIGHT = 4;
+    private static int score = 0;
+    private static int moves = 0;
 
     public static String intToPaddedStr(int i) {
         if (i != 0) {
@@ -26,7 +28,7 @@ public class Main {
                     return str;
             }
         }
-        return "  - ";
+        return "    ";
     }
 
     public static void drawGrid(int[][] grid) {
@@ -39,6 +41,135 @@ public class Main {
         }
 
 
+    }
+
+    private static boolean checkMoves(int[][] grid) {
+        if (moveGrid('w',grid,false)) return true;
+        if (moveGrid('a',grid,false)) return true;
+        if (moveGrid('s',grid,false)) return true;
+        if (moveGrid('d',grid,false)) return true;
+        return false;
+    }
+
+    private static boolean checkWinCon(int[][] grid) {
+        for (int[] row : grid) {
+            for (int cell : row) {
+                if (cell == 2048) return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean moveGrid(char in, int[][] grid, boolean countScore) {
+        boolean somethingHapened = false;
+        int shiftX = 0;
+        int shiftY = 0;
+        int jConst = 0;
+        int iConst = 0;
+        int multX = 0;
+        int multY = 0;
+        int endX = 0;
+        int endY = 0;
+
+        switch (in) {
+
+            case 'd':
+                jConst = 0;
+                iConst = 0;
+                shiftX = 1;
+                shiftY = 1;
+                multX = 1;
+                multY = 0;
+                endX = WIDTH-1;
+                endY = HEIGHT;
+                break;
+
+            case 'a':
+                jConst = 3;
+                iConst = 0;
+                shiftX = -1;
+                shiftY = 1;
+                multX = 1;
+                multY = 0;
+                endX = WIDTH-1;
+                endY = HEIGHT;
+                break;
+
+            case 's':
+                jConst = 0;
+                iConst = 0;
+                shiftX = 1;
+                shiftY = 1;
+                multX = 0;
+                multY = 1;
+                endX = WIDTH-1;
+                endY = HEIGHT;
+                break;
+
+            case 'w':
+                jConst = 3;
+                iConst = 3;
+                shiftX = -1;
+                shiftY = -1;
+                multX = 0;
+                multY = 1;
+                endX = WIDTH-1;
+                endY = HEIGHT;
+                break;
+
+        }
+
+        for (int i = 0; i < endY; i++) {
+            for (int j = 0; j < endX; j++) {
+                //System.out.println();
+                //System.out.println((iConst+i*shiftY)*multX + " + " + (jConst+j*shiftX)*multY);
+                //System.out.println((iConst+i*shiftY)*multY + " + " + (jConst+j*shiftX)*multX);
+                int finalI = (iConst+i*shiftY)*multX + (jConst+j*shiftX)*multY;
+                int finalJ = (iConst+i*shiftY)*multY + (jConst+j*shiftX)*multX;
+                //System.out.println(finalI);
+                //System.out.println(finalJ);
+                int finalShiftX = shiftX*multX;
+                int finalShiftY = shiftY*multY;
+                if (IntStream.of(powers).anyMatch(x -> x == grid[finalI][finalJ]
+                        + grid[finalI + finalShiftY][finalJ + finalShiftX])) {
+                    //if (Arrays.asList(powers).contains((grid[finalI][finalJ] + grid[finalI][j+1]))) {
+                    grid[finalI+finalShiftY][finalJ+finalShiftX] = grid[finalI+finalShiftY][finalJ+finalShiftX] + grid[finalI][finalJ];
+                    if (countScore) score = score + grid[finalI+finalShiftY][finalJ+finalShiftX];
+                    grid[finalI][finalJ] = 0;
+                    somethingHapened = true;
+                }
+                else {
+                    if ( grid[finalI+finalShiftY][finalJ+finalShiftX] == 0) {
+                        grid[finalI+finalShiftY][finalJ+finalShiftX] = grid[finalI][finalJ];
+                        grid[finalI][finalJ] = 0;
+                        somethingHapened = true;
+                    }
+                }
+            }
+
+        }
+
+        for (int i = 0; i < endY; i++) {
+            for (int j = 0; j < endX; j++) {
+                //System.out.println();
+                //System.out.println((iConst+i*shiftY)*multX + " + " + (jConst+j*shiftX)*multY);
+                //System.out.println((iConst+i*shiftY)*multY + " + " + (jConst+j*shiftX)*multX);
+                int finalI = (iConst+i*shiftY)*multX + (jConst+j*shiftX)*multY;
+                int finalJ = (iConst+i*shiftY)*multY + (jConst+j*shiftX)*multX;
+                //System.out.println(finalI);
+                //System.out.println(finalJ);
+                int finalShiftX = shiftX*multX;
+                int finalShiftY = shiftY*multY;
+                if ( grid[finalI+finalShiftY][finalJ+finalShiftX] == 0) {
+                    grid[finalI+finalShiftY][finalJ+finalShiftX] = grid[finalI][finalJ];
+                    grid[finalI][finalJ] = 0;
+                    somethingHapened = true;
+                }
+
+            }
+
+        }
+        return somethingHapened;
     }
 
     public static void countEmpty(int[][] grid, List<IndexPair> emptyList) {
@@ -56,6 +187,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+
         Random rd = new Random();
 
         Scanner sc = new Scanner(System.in);
@@ -65,131 +197,47 @@ public class Main {
         System.out.println("Hello and welcome!");
         char in = ' ';
         while (in != 'q') {
+
             System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             countEmpty(grid,empty);
             //System.out.println(empty);
-            Collections.shuffle(empty);
+            Collections.shuffle(empty,rd);
             //System.out.println(empty);
             if (!empty.isEmpty()) {
                 grid[empty.get(0).y()][empty.get(0).x()] = 2;
-            }
-
-            int shiftX = 0;
-            int shiftY = 0;
-            int jConst = 0;
-            int iConst = 0;
-            int multX = 0;
-            int multY = 0;
-            int endX = 0;
-            int endY = 0;
-
-            switch (in) {
-
-                case 'd':
-                    jConst = 0;
-                    iConst = 0;
-                    shiftX = 1;
-                    shiftY = 1;
-                    multX = 1;
-                    multY = 0;
-                    endX = 3;
-                    endY = 4;
-                    break;
-
-                case 'a':
-                    jConst = 3;
-                    iConst = 0;
-                    shiftX = -1;
-                    shiftY = 1;
-                    multX = 1;
-                    multY = 0;
-                    endX = 3;
-                    endY = 4;
-                    break;
-
-                case 's':
-                    jConst = 0;
-                    iConst = 0;
-                    shiftX = 1;
-                    shiftY = 1;
-                    multX = 0;
-                    multY = 1;
-                    endX = 3;
-                    endY = 4;
-                    break;
-
-                case 'w':
-                    jConst = 3;
-                    iConst = 3;
-                    shiftX = -1;
-                    shiftY = -1;
-                    multX = 0;
-                    multY = 1;
-                    endX = 3;
-                    endY = 4;
-                    break;
-
-            }
-
-            for (int i = 0; i < endY; i++) {
-                for (int j = 0; j < endX; j++) {
-                    //System.out.println();
-                    //System.out.println((iConst+i*shiftY)*multX + " + " + (jConst+j*shiftX)*multY);
-                    //System.out.println((iConst+i*shiftY)*multY + " + " + (jConst+j*shiftX)*multX);
-                    int finalI = (iConst+i*shiftY)*multX + (jConst+j*shiftX)*multY;
-                    int finalJ = (iConst+i*shiftY)*multY + (jConst+j*shiftX)*multX;
-                    //System.out.println(finalI);
-                    //System.out.println(finalJ);
-                    int finalShiftX = shiftX*multX;
-                    int finalShiftY = shiftY*multY;
-                    if (IntStream.of(powers).anyMatch(x -> x == grid[finalI][finalJ]
-                            + grid[finalI + finalShiftY][finalJ + finalShiftX])) {
-                        //if (Arrays.asList(powers).contains((grid[finalI][finalJ] + grid[finalI][j+1]))) {
-                        grid[finalI+finalShiftY][finalJ+finalShiftX] = grid[finalI+finalShiftY][finalJ+finalShiftX] + grid[finalI][finalJ];
-                        grid[finalI][finalJ] = 0;
-                    }
-                    else {
-                        if ( grid[finalI+finalShiftY][finalJ+finalShiftX] == 0) {
-                            grid[finalI+finalShiftY][finalJ+finalShiftX] = grid[finalI][finalJ];
-                            grid[finalI][finalJ] = 0;
-                        }
-                    }
+            } else {
+                int [][] testGrid =  new int[WIDTH][HEIGHT];
+                for (int i = 0; i < WIDTH; i++) {
+                    System.arraycopy(grid[i],0,testGrid[i],0,grid.length);
                 }
-
+                System.out.println(Arrays.deepToString(testGrid));
+                if (!checkMoves(testGrid)) {
+                    drawGrid(grid);
+                    System.out.println("No more moves, you've lost!");
+                    break;
+                };
             }
 
-            for (int i = 0; i < endY; i++) {
-                for (int j = 0; j < endX; j++) {
-                    //System.out.println();
-                    //System.out.println((iConst+i*shiftY)*multX + " + " + (jConst+j*shiftX)*multY);
-                    //System.out.println((iConst+i*shiftY)*multY + " + " + (jConst+j*shiftX)*multX);
-                    int finalI = (iConst+i*shiftY)*multX + (jConst+j*shiftX)*multY;
-                    int finalJ = (iConst+i*shiftY)*multY + (jConst+j*shiftX)*multX;
-                    //System.out.println(finalI);
-                    //System.out.println(finalJ);
-                    int finalShiftX = shiftX*multX;
-                    int finalShiftY = shiftY*multY;
-                        if ( grid[finalI+finalShiftY][finalJ+finalShiftX] == 0) {
-                            grid[finalI+finalShiftY][finalJ+finalShiftX] = grid[finalI][finalJ];
-                            grid[finalI][finalJ] = 0;
-                        }
+            if (moveGrid(in, grid,true)) moves++;
+            //grid[0][0] = 1024;
+            //grid[0][1] = 1024;
+            for (int i = 0; i < HEIGHT; i++) {
+                for (int j = 0; j < WIDTH; j++) {
+                    grid[i][j]= 12;
 
                 }
-
             }
-
-
-
-
+            System.out.println("Score: " + score);
+            System.out.println("Moves: " + moves);
             drawGrid(grid);
+            if (checkWinCon(grid)) {
+                System.out.println("Congratulations! You won in " + moves + " moves!");
+            }
+
             in = sc.next().charAt(0);
-
-
-
-
-
-
 
         }
     }
+
+
 }
